648ed2dfef91ed8748dc4fd2726978125837d3ac
Luis <hairchevere@gmail.com>
2023-02-28 10:57:05 -0600

    added update function for thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -36,5 +36,19 @@ module.exports = {
         )
         .catch((err) => res.status(500).json(err))
     },
+    //updates a thought
+    updateThought(req, res) {
+        Thought.findOneAndUpdate(
+            {_id: req.params.thoughtId},
+            {$set: req.body},
+            {runValidators: true, New: true}
+        )
+        .then((user) =>
+        !user
+        ? res.status(404).json({message: "No thought found with ID!"})
+        : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
     
 }
