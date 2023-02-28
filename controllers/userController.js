0f48674d5afa2d8372c9e30d82445db102384fc2
Luis <hairchevere@gmail.com>
2023-02-28 11:24:06 -0600

    created get single user function

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,9 +2,23 @@ const { User, Thought } = require("../models");
 
 module.exports = {
     //Gets all users
-    getUSer(req, res) {
+    getUser(req, res) {
         User.find({})
         .then((user) => res.json(user))
         .catch((err) => res.status(500).json(err));
-    }
+    },
+    //Get single user
+    getSingleUser(req, res) {
+        User.findOne({_id: req.params.userId})
+        .populate("thoughts")
+        .populate("friends")
+        .select("-__v")
+        .then((user) =>
+        !user
+        ? res.status(404).json({message: "No user found with ID!"})
+        : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+    
 }
