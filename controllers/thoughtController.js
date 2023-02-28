dc631e6e59e123138af3c2a421e605d53c8ad084
Luis <hairchevere@gmail.com>
2023-02-28 10:50:36 -0600

    created the create though function

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -4,20 +4,37 @@ module.exports = {
     //Get all thoughts
     getThought(req, res) {
         Thought.find({})
-        .then((thought) => res.json(thought))
-        .catch((err) => res.status(500).json(err));
+            .then((thought) => res.json(thought))
+            .catch((err) => res.status(500).json(err));
     },
 
     //Gets a single thought
     getSingleThough(req, res) {
-        Thought.findOne({_id: req.params.thoughtId})
+        Thought.findOne({ _id: req.params.thoughtId })
             .select("-__V")
             .then((thought) =>
-            !thought
-            ? res.status(404).json({ message: "No Thought with this ID"})
-            : res.json(thought)
+                !thought
+                    ? res.status(404).json({ message: "No Thought with this ID" })
+                    : res.json(thought)
             )
             .catch((err) => res.status(500).json(err));
+    },
+    // create a thought and push created thought's _id to the associated users thoughts array field
+    createThought(req, res) {
+        Thought.create(req.body)
+        .then(({_id}) => {
+            return User.findOneAndUpdate(
+                {_id: req.body.userId},
+                {$push: { thoughts: _id}},
+                {new: true}
+            );
+        })
+        .then((thought) => 
+        !thought
+        ? res.status(404).json({message: "No user found with ID!"})
+        :res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err))
     },
     
 }
