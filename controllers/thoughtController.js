764b2045bf262aa62b1e17e85bf2855fb88b460b
Luis <hairchevere@gmail.com>
2023-02-28 11:07:24 -0600

    create reaction function

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -69,5 +69,17 @@ module.exports = {
         )
         .catch((err) => res.status(500).json(err));
     },
-    
+    createReaction(req, res) {
+        Thought.findOneAndUpdate(
+            {_id: req.params.thoughtId},
+            {$addToSet: {reactions: req.body}},
+            {runValidators: true, new: true}
+        )
+        .then((thought) =>
+        !thought
+        ? res.status(404).json({message: "No thought found with ID!"})
+        : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
 }
